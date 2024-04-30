e203759a336f95acb1c012aaa372c52fd30e91af
Ennio Haibach <ennio.haibach@gmail.com>
2024-04-30 12:20:30 +0200

    Remove test saving functionality

--- src/de/dhbw/advancewars/AdvanceWars.java
+++ src/de/dhbw/advancewars/AdvanceWars.java
@@ -5,35 +5,20 @@ import de.dhbw.advancewars.maps.IMapService;
 import de.dhbw.advancewars.maps.MapService;
 import de.dhbw.advancewars.maps.data.MapDTO;
 import de.dhbw.advancewars.maps.data.MapTile;
-import de.dhbw.advancewars.maps.data.TileType;
 
 public class AdvanceWars{
-    private void saveTestMap() {
-        IMapService mapService = new MapService();
-
-        MapTile[][] tiles = new MapTile[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                tiles[i][j] = new MapTile(TileType.PLAIN, i, j);
-            }
-        }
-        MapDTO map = new MapDTO("TestMap", 10, 10, tiles);
-
-        mapService.saveMap(map, "assets/maps/test.pak0");
-    }
-
-    private void loadTestMap() {
+    private void loadMap(String name) {
         IMapService mapService = new MapService();
 
         try {
-            MapDTO map = mapService.loadMap("assets/maps/test.pak0");
+            MapDTO map = mapService.loadMap("assets/maps/" + name + ".pak0");
             System.out.println("Map name: " + map.getName());
-            System.out.println("Map width: " + map.getWidth());
+            System.out.println("Map width: " + map.getTiles().length);
             System.out.println("Map height: " + map.getHeight());
             System.out.println("Map tiles:");
             for (MapTile[] row : map.getTiles()) {
                 for (MapTile tile : row) {
-                    System.out.print(tile.type() + " ");
+                    System.out.print(tile.type() + " " + tile.x() + " " + tile.y() + " | ");
                 }
                 System.out.println();
             }
@@ -45,8 +30,7 @@ public class AdvanceWars{
     public AdvanceWars(){
         GUI2 gui = new GUI2(this);
 
-        saveTestMap();
-        loadTestMap();
+        loadMap("test");
     }
 
     public static void main(String[] args){
